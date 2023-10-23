5ae4ff407169921c77e04bc23bdfd93a7f41a631
Rabia Gül Baruğ <rabiabrg7@gmail.com>
2023-10-23 18:21:30 +0300

    feat: header and link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,64 @@
 import './App.css';
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import React, {useEffect, useState} from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import axios from "axios";
 import Header from './components/header';
 import Details from './pages/details';
 import AddBlog from './pages/add-blog';
 import Category from './pages/category';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Header/>
-        <Routes>
-          <Route exact path='/' element = {"Homepage"} />
-          <Route path='/details/:id' element = { <Details/>} />
-          <Route path='/addblog' element = { <AddBlog/> } />
-          <Route path='/category/:id' element = {  <Category/> } />
-        </Routes>
-      </Router>
-    </div>
-  );
+    const [blogs, setBlogs] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true)
+
+        async function fetchData() {
+            try {
+                const result = await axios.get("http://localhost:1337/api/blogs")
+                setBlogs(result.data.data)
+                setLoading(false)
+            } catch (error) {
+                setError(error)
+                setLoading(false)
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    return (
+        <div className="App">
+            <Router>
+                <Header/>
+                <Routes>
+                    <Route exact path='/'
+                           element={<div>{blogs.sort((a, b) => b.attributes.createdAt.localeCompare(a.attributes.createdAt)).map(blog => {
+                            return (
+                                <div style={{
+                                    width: "60%",
+                                    margin: "auto",
+                                    padding: "20px",
+                                    marginBottom: "15px",
+                                    border: "1px solid coral",
+                                    borderRadius: "10px"
+                                }}
+                                key={blog.id}>
+                                    <h3>{blog.attributes.title}</h3>
+                                    <h5>{blog.attributes.author}</h5>
+                                    <p>{blog.attributes.body}</p>
+                                </div>
+                            )
+                        })}</div>}/>
+                    <Route path='/details/:id' element={<Details/>}/>
+                    <Route path='/addblog' element={<AddBlog/>}/>
+                    <Route path='/category/:id' element={<Category/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand" >RGB-BLOG</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link to="/allblog" className="nav-link active" aria-current="page">ALL BLOGS</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/addblog">ADD BLOG</Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                               aria-expanded="false">
+                                CATEGORIES
+                            </Link>
+                            <ul className="dropdown-menu">
+                                <li><Link className="dropdown-item" href="#">Action</Link></li>
+                                <li><Link className="dropdown-item" href="#">Another action</Link></li>
+                                <li>
+                                    <hr className="dropdown-divider"/>
+                                </li>
+                                <li><Link className="dropdown-item" href="#">Something else here</Link></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
